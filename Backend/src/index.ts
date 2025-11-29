30ab96b50543391fa95f616895f13a1fe44d8cb9
Nhi Truong <nhi.truong2312514@hcmut.edu.vn>
2025-11-29 12:10:14 +0700

    Feature: Add event creation and management endpoints

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,10 +3,17 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import mentorRoutes from './routes/mentorRoutes';
+<<<<<<< HEAD
 import eventRoutes from './routes/eventRoutes';
+=======
+<<<<<<< HEAD
+>>>>>>> 168e333 (Feature: Add event creation and management endpoints)
 import submissionRoutes from './routes/submissionRoutes';
 
 
+=======
+import eventRoutes from './routes/eventRoutes';
+>>>>>>> feature/change
 dotenv.config();
 
 const app: Express = express();
@@ -16,9 +23,16 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/mentors', mentorRoutes);
+<<<<<<< HEAD
 app.use('/api/events', eventRoutes);
+=======
+<<<<<<< HEAD
+>>>>>>> 168e333 (Feature: Add event creation and management endpoints)
 app.use('/api/submissions', submissionRoutes);
 
+=======
+app.use('/api/events', eventRoutes);
+>>>>>>> feature/change
 app.get('/', (req: Request, res: Response) => {
   res.send('Server is running!');
 });
